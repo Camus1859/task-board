749e8104b99b75dc5c081f7fd0a7f85d620221fa
Anderson Laventure <Laventurepsu1@gmail.com>
2026-05-22 20:02:48 -0400

    refactor: replace inline task form with task-form component in task-board

--- task-board/src/task-board.ts
+++ task-board/src/task-board.ts
@@ -1,6 +1,7 @@
 import { customElement, state, property, query } from "lit/decorators.js";
 import { LitElement, html, css } from "lit";
 import "./task-column.ts";
+import "./task-form.ts";
 
 @customElement("task-board")
 export class TaskBoard extends LitElement {
@@ -49,46 +50,11 @@ export class TaskBoard extends LitElement {
       status: "Done",
     },
   ];
-
-  @state()
-  title: string = "";
-
-  @state()
-  description: string = "";
-
-  @state()
-  priority: string = "";
-
-  @query("input", true) _input!: HTMLInputElement;
-
   render() {
     return html`
-      <div class="form-container">
-        Title:
-        <input
-          .value=${this.title}
-          @input=${(e: Event) =>
-            (this.title = (e.target as HTMLInputElement).value)}
-        />
-        Description:
-        <input
-          .value=${this.description}
-          @input=${(e: Event) =>
-            (this.description = (e.target as HTMLInputElement).value)}
-        />
-        Priority:
-        <select
-          .value=${this.priority}
-          @change=${(e: Event) =>
-            (this.priority = (e.target as HTMLSelectElement).value)}
-        >
-          <option value="high">High</option>
-          <option value="medium">Medium</option>
-          <option value="low">Low</option>
-        </select>
-        <button @click=${this._addTask}>Add Task</button>
-      </div>
       <div class="column-container">
+        <task-form @task-added=${this._handleTaskAdded}></task-form>
+
         <task-column
           name="To Do"
           .tasks=${this.tasks.filter((task) => task.status === "To Do")}
@@ -105,20 +71,7 @@ export class TaskBoard extends LitElement {
     `;
   }
 
-  private _addTask() {
-    this.tasks = [
-      ...this.tasks,
-      {
-        id: Math.random().toString(),
-        title: this.title,
-        description: this.description,
-        priority: this.priority,
-        status: "To Do",
-      },
-    ];
-
-    this.title = "";
-    this.description = "";
-    this.priority = "";
+  _handleTaskAdded(e: CustomEvent) {
+    this.tasks = [...this.tasks, e.detail.task];
   }
 }
