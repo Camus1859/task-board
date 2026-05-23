de0ae9204b57e760d30d4b4e87d37de6d28aedc0
Anderson Laventure <Laventurepsu1@gmail.com>
2026-05-23 16:51:57 -0400

    feat: enhance task-form with validation and error handling for inputs

--- task-board/src/task-form.ts
+++ task-board/src/task-form.ts
@@ -3,7 +3,34 @@ import { customElement, state } from "lit/decorators.js";
 
 @customElement("task-form")
 export class TaskForm extends LitElement {
-  static styles = css``;
+  static styles = css`
+    .form-container {
+      display: flex;
+      flex-direction: column;
+      max-width: 300px;
+    }
+
+    .task-button {
+      width: 90px;
+      padding: 10px;
+    }
+
+    .description {
+      height: 100px;
+    }
+
+    .priority {
+      width: 100px;
+    }
+
+    .space {
+      margin-bottom: 10px;
+    }
+
+    .error {
+      color: red;
+    }
+  `;
 
   @state()
   title: string = "";
@@ -14,22 +41,39 @@ export class TaskForm extends LitElement {
   @state()
   priority: string = "";
 
+  @state()
+  error: { title: string; description: string; priority: string } = {
+    title: "",
+    description: "",
+    priority: "",
+  };
+
   render() {
     return html` <div class="form-container">
       Title:
       <input
+        class="title"
         .value=${this.title}
         @input=${(e: Event) =>
           (this.title = (e.target as HTMLInputElement).value)}
       />
+      ${this.error.title ? html`<p class="error">${this.error.title}</p>` : ""}
+      <div class="space"></div>
       Description:
       <input
+        class="description"
         .value=${this.description}
         @input=${(e: Event) =>
           (this.description = (e.target as HTMLInputElement).value)}
       />
+      ${this.error.description
+        ? html`<p class="error">${this.error.description}</p>`
+        : ""}
+      <div class="space"></div>
+
       Priority:
       <select
+        class="priority"
         .value=${this.priority}
         @change=${(e: Event) =>
           (this.priority = (e.target as HTMLSelectElement).value)}
@@ -38,11 +82,39 @@ export class TaskForm extends LitElement {
         <option value="medium">Medium</option>
         <option value="low">Low</option>
       </select>
-      <button @click=${this._addTask}>Add Task</button>
+      ${this.error.priority
+        ? html`<p class="error">${this.error.priority}</p>`
+        : ""}
+      <div class="space"></div>
+
+      <button class="task-button" @click=${this._addTask}>Add Task</button>
     </div>`;
   }
 
+  private _validation() {
+    this.error = { title: "", description: "", priority: "" };
+    let hasError = false;
+    if (this.title.trim() === "") {
+      this.error = { ...this.error, title: "Title is missing" };
+      hasError = true;
+    }
+    if (this.description.trim() === "") {
+      this.error = { ...this.error, description: "Description is missing" };
+      hasError = true;
+    }
+    if (this.priority.trim() === "") {
+      this.error = { ...this.error, priority: "Priority is missing" };
+      hasError = true;
+    }
+    return hasError;
+  }
+
   private _addTask() {
+    const hasError = this._validation();
+    if (hasError) {
+      return;
+    }
+
     const task = {
       id: Math.random().toString(),
       title: this.title,
